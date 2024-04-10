680d7bced488aad4f27dc4836767aeeb8cc9fe60
alberto <alberto.corrias@gmail.com>
2024-04-10 14:29:35 +0800

    readding db sqlite3 database and switching back the settings

--- workload_management/workload_management/settings.py
+++ workload_management/workload_management/settings.py
@@ -77,14 +77,12 @@ WSGI_APPLICATION = 'workload_management.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'OPTIONS': {
-        'service': 'workload_service',
-        'passfile': '.pgpass',
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
+    }
 }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
