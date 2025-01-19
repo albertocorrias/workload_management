ea4ec771e04b470c6229faf86d323bb90b14f950
alberto <alberto.corrias@gmail.com>
2025-01-19 14:11:46 +0800

    failed attempt at extracting data from pdf

--- workload_management/workload_app/read_pdf.py
+++ workload_management/workload_app/read_pdf.py
@@ -1,4 +1,6 @@
 import pymupdf # imports the pymupdf library
+import math
+
 doc = pymupdf.open("course_report.pdf") # open a document
 text = ''
 for page in doc: # iterate the document pages
@@ -10,8 +12,6 @@ start = text.find('COURSE LEARNING OUTCOMES')
 
 extracted = text[start:end]
 extracted_list = extracted.split('\n')
-
-
 
 all_mlo_data = []
 
@@ -36,3 +36,32 @@ while(response_count_pos < len(extracted_list)):
 
 print(all_mlo_data)
 
+item = all_mlo_data[0]
+
+resp_cnt = int(item['response_count'])
+avg = float(item['mean'])
+std = float(item['standard_deviation'])
+
+distributions = []
+for n_four in range(0,resp_cnt-4):
+    for n_three in range(0,resp_cnt-4):
+        for n_two in range(0,resp_cnt-4):
+            n_one = resp_cnt - n_four-n_three-n_two
+            calc_mean = (n_four*4+n_three*3+n_two*2+n_one)/resp_cnt
+            calc_std = math.sqrt((n_four*(4 - calc_mean)**2 + n_three*(3 - calc_mean)**2 +\
+                       n_two*(2-calc_mean)**2 + (n_one - calc_mean)**2)/resp_cnt)
+            #print(calc_std)
+            if (abs(calc_mean - avg) < 0.05 and abs(calc_std - std) < 0.05):
+                dist = {
+                    'fours' : n_four,
+                    'threes' : n_three,
+                    'twos' : n_two,
+                    'ones' : n_one,
+                    'avg' :  calc_mean,
+                    'std' : calc_std
+                    }
+                distributions.append(dist)
+print(len(distributions))
+            
+            
+            
